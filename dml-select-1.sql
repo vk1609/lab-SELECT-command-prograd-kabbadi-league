666e64bdb143acae2e009c0a2faf45fcfafed926
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-17 10:06:01 +0530

    Create dml-select-1.sql

---
+++ dml-select-1.sql
@@ -0,0 +1,22 @@
+## PROGRESSION 1:
+
+1. Insert skill
+2. Insert city
+3. Insert venue
+4. Insert team
+5. Insert player
+6. Insert outcome
+7. Insert innings
+8. Insert game
+9. Insert umpire
+10. Insert umpire_type
+11. Insert game_umpire_type
+12. Insert wicket_type
+13. Insert delivery
+14. Insert wicket
+15. Insert extratype
+16. Insert extradelivery
+17. Update venue
+18. Update team
+19. Update player
+20. Delete player
