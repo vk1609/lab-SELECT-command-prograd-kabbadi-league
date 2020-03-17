ec6d588d484ef6ef5c8f52e155c3dda77fa66601
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-17 10:07:28 +0530

    Update dml-select-1.sql

--- dml-select-1.sql
+++ dml-select-1.sql
@@ -1,22 +1,42 @@
-## PROGRESSION 1:
+## Progression
 
-1. Insert skill
-2. Insert city
-3. Insert venue
-4. Insert team
-5. Insert player
-6. Insert outcome
-7. Insert innings
-8. Insert game
-9. Insert umpire
-10. Insert umpire_type
-11. Insert game_umpire_type
-12. Insert wicket_type
-13. Insert delivery
-14. Insert wicket
-15. Insert extratype
-16. Insert extradelivery
-17. Update venue
-18. Update team
-19. Update player
-20. Delete player
+
+-- 1. **Write a query to display the team details. Display the records sorted in the order of team name.**
+
+-- 2. **Write a query to display the player details whose name starts with a letter `S` and who do not belong to the country 'South Africa'. Display the records sorted in descending order based on player name.**
+
+-- 3. **Write a query to display the event number and the seconds taken for each event. Display the records sorted in ascending order based on event number.**
+
+-- 4. **Write a query to display the event numbers of those events which has sum of raid points and defending points greater than or equal to 3. Display the records sorted in ascending order based on event_no.**
+
+-- 5. **Write a query to display the name of all the players who belong to the country India or Iran. Display the records sorted in ascending order based on player name.**
+
+-- 6. **Write a query to display the name of all the players who does not belong to the country India and Iran. Display the records sorted in ascending order based on player name.**
+
+-- 7. **Write a query to display the names of all the skills that start with `Defend`. Display the records sorted in ascending order based on skill name.**
+
+-- 8. **Write a query to display all the stadium names that ends with `Complex`. Display the records sorted in ascending order based on stadium names.**
+
+-- 9. **Write a query to display the winner team ids and the scores of those teams whose scores are less than 35. Display the records sorted in descending order based on outcome id.**
+
+-- 10. **Write a query to display the game details of those games that were played between January and March of 2020. Display the records sorted in descending order based on id.**
+
+-- 11. **Write a query to display the names of players who are Raiders. Display the records sorted in ascending order based on player name.**
+
+-- 12. **Write a query to display the event number, raid points and defence points scored in the first innings of the game that was played on `2020-01-26`. Display the records sorted in ascending order based on event number.**
+
+-- 13. **Write a query to display the name of the winner team in the game played on `2020-03-09`. Display the records sorted in ascending order based on team name.**
+
+-- 14. **Write a query to display the names of players who were the player of the match in the game played on 2020-03-09'. Display the records sorted in ascending order based on player name.**
+
+-- 15. **Write a query to display the names of players who are All rounders. Display the records sorted in ascending order based on player name.**
+
+-- 16. **Write a query to display the venue names for all the games that were played on `2020-03-09`. Display the records sorted in ascending order based on venue name.**
+
+-- 17. **Write a query to display the Coach's name of the team `Iran`**
+
+-- 18. **Write a query to display the event number and the defence points of `Fazel Atrachali` in each event. Display the records sorted in ascending order based on event number.**
+
+-- 19. **Write a query to display the names of the Winning teams in the month of March 2020. Display the records sorted in ascending order based on team name.**
+
+-- 20. **Write a query to display the names of players who were the player of the match in the games held in March 2020. Display the records sorted in ascending order based on player name.**
